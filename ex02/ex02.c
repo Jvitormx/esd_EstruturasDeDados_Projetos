f637434c85a633a54d26055e40cad9b45864246a
joao xavier <jvitormx@gmail.com>
2024-08-28 13:14:27 -0300

    update ex02.c

--- ex02/ex02.c
+++ ex02/ex02.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 
+// Definindo quantidade de perguntas ao questionario | plano 2D
 #define N_RESPOSTAS 2
 
 typedef int respQuestionario;
@@ -13,14 +14,15 @@ struct pessoa
     respQuestionario respostas[N_RESPOSTAS];
 };
 
+// definindo tipo pessoa e ponteiro para tipo pessoa
 typedef struct pessoa TPessoa;
-typedef struct pessoa *PTPessoa;
+typedef TPessoa *PTPessoa;
+
+PTPessoa inicializarPessoas(char **vNomAux, int n_pessoas);
 
 float calcularDistancia(respQuestionario respostasP1[], respQuestionario respostasP2[]);
 
 void encontrarCompatibilidade(char *nome, TPessoa pessoas[], int lim_distancia, int n_pessoas);
-
-PTPessoa inicializarPessoas(char **vNomAux, int n_pessoas);
 
 void desalocarNomes(PTPessoa pessoas, int n_pessoas);
 
@@ -30,18 +32,25 @@ int main(void)
                        "Gabriel", "Camila", "Sandra", "Rafael", "Bruno", "Mariana",
                        "Leonardo", "Mateus", "Vitor", "Davi"};
 
+    // criando ponteiro para struct pessoas
     PTPessoa pessoas;
 
     char nome_pessoa[100];
     int i, n_pessoas;
     float limiar;
 
-    printf("Quantidade de pessoas: ");
+    printf("Quantidade de pessoas [MAX 20]: ");
     scanf("%d", &n_pessoas);
 
-    // inicializa pessoas com nomes e respostas
+    if (n_pessoas > 20)
+    {
+        return 1;
+    }
+
+    // atribuindo ao ponteiro criado o endereco que aponta para as structs pessoas inicializadas com nomes e respostas
     pessoas = inicializarPessoas(vNomAux, n_pessoas);
 
+    // exibindo pessoas e suas respostas
     for (i = 0; i < n_pessoas; i++)
     {
         printf("pessoa %d: %s - resp1: %d | resp2: %d\n", i + 1, pessoas[i].nome, pessoas[i].respostas[0], pessoas[i].respostas[1]);
@@ -53,21 +62,51 @@ int main(void)
     printf("Limiar de compatibilidade: ");
     scanf("%f", &limiar);
 
-    // mostra pessoas compativeis com pessoa escolhida
+    // exibindo pessoas compativeis com pessoa escolhida
     encontrarCompatibilidade(nome_pessoa, pessoas, limiar, n_pessoas);
 
+    // desalocando memoria atribuida para cada nome alocado nas structs pessoas
     desalocarNomes(pessoas, n_pessoas);
 
+    // desalocando memoria atribuida para as structs pessoas
     free(pessoas);
+
+    return 0;
 }
 
-void desalocarNomes(PTPessoa pessoas, int n_pessoas)
+PTPessoa inicializarPessoas(char **vNomAux, int n_pessoas)
 {
-    int i;
+    int i, j, n_rand;
+
+    // criando ponteiro para struct pessoas
+    PTPessoa pessoas;
+
+    // alocando memória para o ponteiro com base no numero determinado de pessoas
+    pessoas = (PTPessoa)malloc(sizeof(TPessoa) * n_pessoas);
+
     for (i = 0; i < n_pessoas; i++)
     {
-        free(pessoas[i].nome);
+        // determinando valor aleatorio com base no numero determinado de pessoas
+        n_rand = rand() % (n_pessoas);
+
+        // alocando memoria para nome no struct pessoa com base no nome aleatorio do vetor de nomes
+        pessoas[i].nome = (char *)malloc(sizeof(strlen(vNomAux[n_rand]) + 1) * sizeof(char));
+
+        // atribuindo nome do vetor de nomes a struct pessoa
+        strcpy(pessoas[i].nome, vNomAux[n_rand]);
+
+        // Atribuindo identificador para diferenciar nomes
+        pessoas[i].nome[strlen(pessoas[i].nome)] = (rand() % 10) + '0';
+
+        // atribuindo respostas aleatorias para struct pessoa
+        for (j = 0; j < N_RESPOSTAS; j++)
+        {
+            pessoas[i].respostas[j] = rand() % 10;
+        }
     }
+
+    // retornando ponteiro pessoas
+    return pessoas;
 }
 
 float calcularDistancia(respQuestionario respostasP1[], respQuestionario respostasP2[])
@@ -81,10 +120,13 @@ void encontrarCompatibilidade(char *nome, TPessoa pessoas[], int lim_distancia, 
     int i, j;
     float distancia;
 
+    // criando ponteiro para struct da pessoa escolhida
     PTPessoa pessoa_escolhida;
+
+    // alocando memoria para pessoa escolhida
     pessoa_escolhida = (PTPessoa)malloc(sizeof(TPessoa));
 
-    // pegando valores da pessoa escolhida
+    // atribuindo valores das respostas da pessoa escolhida que esta no vetor de pessoas a struct pessoa criada para a pessoa escolhida
     for (i = 0; i < n_pessoas; i++)
     {
         if (strcmp(nome, pessoas[i].nome) == 0)
@@ -96,40 +138,31 @@ void encontrarCompatibilidade(char *nome, TPessoa pessoas[], int lim_distancia, 
         }
     }
 
+    // calculando distancia entre as respostas da pessoa escolhida e as demais respostas do vetor pessoas
     for (i = 0; i < n_pessoas; i++)
     {
         if (strcmp(nome, pessoas[i].nome) != 0)
         {
-            // calculando distancia entre as respostas da pessoa escolhida e as demais respostas
             distancia = calcularDistancia(pessoa_escolhida->respostas, pessoas[i].respostas);
+
+            // verificando se distancia de determinada pessoa em relacao a pessoa escolhida se enquadra no limiar determinado
             if (distancia < lim_distancia)
             {
-                printf("nome: %s\n", pessoas[i].nome);
+                // exibindo nome e respostas de cada pessoa que se encaixa no limiar determinado
+                printf("nome: %s - resp1: %d | resp2: %d\n", pessoas[i].nome, pessoas[i].respostas[0], pessoas[i].respostas[1]);
             }
         }
     }
 
+    // desalocando memoria da struct pessoa da pessoa escolhida
     free(pessoa_escolhida);
 }
 
-PTPessoa inicializarPessoas(char **vNomAux, int n_pessoas)
+void desalocarNomes(PTPessoa pessoas, int n_pessoas)
 {
-    int i, j, n_rand;
-    PTPessoa pessoas;
-
-    pessoas = (PTPessoa)malloc(sizeof(TPessoa) * n_pessoas);
-
+    int i;
     for (i = 0; i < n_pessoas; i++)
     {
-        n_rand = rand() % (n_pessoas);
-        pessoas[i].nome = (char *)malloc(sizeof(strlen(vNomAux[n_rand]) + 1) * sizeof(char));
-        strcpy(pessoas[i].nome, vNomAux[n_rand]);
-
-        for (j = 0; j < N_RESPOSTAS; j++)
-        {
-            pessoas[i].respostas[j] = rand() % 10;
-        }
+        free(pessoas[i].nome);
     }
-
-    return pessoas;
 }
